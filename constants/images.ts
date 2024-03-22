682d59e6252425f10a4a5a683a06a9aee320f578
CezGain <benjamin.vrnt@gmail.com>
2024-03-22 19:42:04 +0100

    :art: cleaning

--- constants/images.ts
+++ constants/images.ts
@@ -1,21 +1,7 @@
 const logo = require('../assets/images/logo.png');
-const profileimage = require('../assets/images/profileimage.png');
-const profilepicture = require('../assets/images/profilepicture.png');
-const profilecroneur = require('../assets/images/profilecroneur.png');
-const profileconnected = require('../assets/images/profileconnected.png');
-const profilevision = require('../assets/images/profilevision.png');
-const macron = require('../assets/images/macron.png');
-const test = require('../assets/images/test.gif');
 
 const IMAGES = {
-  logo,
-  profileimage,
-  profilepicture,
-  profilecroneur,
-  profileconnected,
-  profilevision,
-  macron,
-  test
+  logo
 };
 
 export default IMAGES;
