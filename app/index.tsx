e564f784b13aa12d105a9d01dfc445c0e9c182df
CezGain <benjamin.vrnt@gmail.com>
2024-03-22 16:25:00 +0100

    push

--- app/index.tsx
+++ app/index.tsx
@@ -5,8 +5,6 @@ import { useTokenStore } from 'hooks/store/useTokenStore';
 export default () => {
   const { token } = useTokenStore();
 
-  return <Redirect href="/a/register/public-data" />;
-
   if (token) return <Redirect href="/a/home" />;
 
   return <Redirect href="/a/login" />;
