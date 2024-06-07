adf65759ba98b9d6d047e1b4ba0cff00937af07c
CezGain <benjamin.vrnt@gmail.com>
2024-06-07 10:09:44 +0200

    style: commentary in types.ts

--- constants/types.ts
+++ constants/types.ts
@@ -1,7 +1,11 @@
+// All form typescript types
+
 export type LoginForm = {
   identifier: string;
   password: string;
 };
+
+// All api typescript types
 
 export type HttpMethod = 'GET' | 'POST' | 'PUT' | 'DELETE' | 'PATCH';
 
