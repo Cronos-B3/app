019d87e1b177b29d6e438840bf5ab2b825e24439
CezGain <benjamin.vrnt@gmail.com>
2024-06-12 10:22:20 +0200

    feat: early home page

--- app/(app)/a/(private)/home.tsx
+++ app/(app)/a/(private)/home.tsx
@@ -1,3 +1,5 @@
+import HomePage from '@/components/pages/HomePage';
+
 export default function Home() {
-  return <></>;
+  return <HomePage />;
 }
---
+++ components/pages/HomePage.tsx
@@ -0,0 +1,7 @@
+import { YStack } from 'tamagui';
+
+export default function HomePage() {
+  if (__DEV__) console.log('📃 - HomePage');
+
+  return <YStack></YStack>;
+}
