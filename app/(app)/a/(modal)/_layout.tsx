9b0983162a59d281087478d994d0d480613c5148
CezGain <benjamin.vrnt@gmail.com>
2024-06-13 16:51:28 +0200

    style: animation now done by the layout

--- app/(app)/a/(modal)/_layout.tsx
+++ app/(app)/a/(modal)/_layout.tsx
@@ -1,17 +1,20 @@
-import { Stack } from 'expo-router';
+import { router, Stack } from 'expo-router';
+import { Stack as StackT } from 'tamagui';
 
 export default () => {
   return (
-    <Stack
-      screenOptions={{
-        headerShown: false,
-        animation: 'none',
-        contentStyle: { backgroundColor: 'transparent' },
-      }}>
-      <Stack.Screen name="menu" />
-      <Stack.Screen name="settings" />
-      <Stack.Screen name="search" />
-      <Stack.Screen name="post" />
-    </Stack>
+    <StackT flex={1} onPress={() => router.back()}>
+      <Stack
+        screenOptions={{
+          headerShown: false,
+          animation: 'slide_from_bottom',
+          contentStyle: { backgroundColor: 'transparent', justifyContent: 'flex-end' },
+        }}>
+        <Stack.Screen name="menu" />
+        <Stack.Screen name="settings" />
+        <Stack.Screen name="search" />
+        <Stack.Screen name="post" />
+      </Stack>
+    </StackT>
   );
 };
--- components/templates/ModalTemplate.tsx
+++ components/templates/ModalTemplate.tsx
@@ -1,14 +1,6 @@
-import React, { useEffect } from 'react';
 import { DEVICE } from '@/constants/config';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import { Button, Stack, YStack, YStackProps } from 'tamagui';
-import Animated, {
-  Easing,
-  useSharedValue,
-  useAnimatedStyle,
-  withTiming,
-  runOnJS,
-} from 'react-native-reanimated';
 import Text from '../atoms/Text';
 import { ArrowLeft } from '@tamagui/lucide-icons';
 import { router } from 'expo-router';
@@ -20,68 +12,37 @@ type ModalTemplateProps = {
 
 const ModalTemplate = YStack.styleable<ModalTemplateProps>(
   ({ children, title, height = 50, ...props }, ref) => {
-    const { bottom: insetBottom } = useSafeAreaInsets();
-
-    const modalHeight = (DEVICE.height * height) / 100 + insetBottom;
-
-    const translateY = useSharedValue(modalHeight);
-
-    useEffect(() => {
-      translateY.value = withTiming(0, {
-        duration: 350,
-        easing: Easing.inOut(Easing.ease),
-      });
-    }, []);
-
-    const animatedStyle = useAnimatedStyle(() => {
-      return {
-        transform: [{ translateY: translateY.value }],
-      };
-    });
-
-    const closeModal = () => {
-      translateY.value = withTiming(
-        modalHeight,
-        {
-          duration: 350,
-          easing: Easing.inOut(Easing.ease),
-        },
-        () => runOnJS(router.back)()
-      );
-    };
+    const { bottom } = useSafeAreaInsets();
 
     return (
-      <Stack flex={1} justifyContent="flex-end" onPress={closeModal}>
-        <Animated.View style={[{ height: modalHeight, paddingBottom: insetBottom }, animatedStyle]}>
-          <YStack
-            onPress={() => null}
-            backgroundColor={'$modalBackground'}
-            paddingHorizontal={'6%'}
-            flex={1}
-            ref={ref}
-            borderTopLeftRadius={'$6'}
-            borderTopRightRadius={'$6'}>
-            <Button
-              color={'$inversed'}
-              icon={<ArrowLeft size={'$6'} />}
-              position="absolute"
-              top={DEVICE.height * 0.05}
-              left={'5%'}
-              onPress={closeModal}
-            />
-            {title && (
-              <Stack height={DEVICE.height * 0.1} justifyContent="center" alignItems="center">
-                <Text fontSize={'$8'} fontFamily={'$bold'}>
-                  {title}
-                </Text>
-              </Stack>
-            )}
-            <YStack flex={1} {...props}>
-              {children}
-            </YStack>
-          </YStack>
-        </Animated.View>
-      </Stack>
+      <YStack
+        height={(DEVICE.height * height) / 100 + bottom}
+        paddingBottom={bottom}
+        onPress={() => null}
+        backgroundColor={'$modalBackground'}
+        paddingHorizontal={'6%'}
+        ref={ref}
+        borderTopLeftRadius={'$6'}
+        borderTopRightRadius={'$6'}>
+        <Button
+          color={'$inversed'}
+          icon={<ArrowLeft size={'$6'} />}
+          position="absolute"
+          top={DEVICE.height * 0.05}
+          left={'5%'}
+          onPress={() => router.back()}
+        />
+        {title && (
+          <Stack height={DEVICE.height * 0.1} justifyContent="center" alignItems="center">
+            <Text fontSize={'$8'} fontFamily={'$bold'}>
+              {title}
+            </Text>
+          </Stack>
+        )}
+        <YStack flex={1} {...props}>
+          {children}
+        </YStack>
+      </YStack>
     );
   }
 );
